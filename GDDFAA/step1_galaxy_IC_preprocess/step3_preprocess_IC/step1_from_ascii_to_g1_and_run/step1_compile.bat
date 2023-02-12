663db95888d9d642996499c5dcb29a0fee753017
darkgaia <43307194+GuJianyu0@users.noreply.github.com>
2023-02-12 17:53:06 +0800

    Add files via upload

---
+++ GDDFAA/step1_galaxy_IC_preprocess/step3_preprocess_IC/step1_from_ascii_to_g1_and_run/step1_compile.bat
@@ -0,0 +1,8 @@
+
+rm *.exe
+
+g++ read_snapshot.cpp ../../../step3_actions/step2_Data_TACT/DataInterface/Gadget2FormatData_io.cpp -o read_snapshot.exe
+
+
+
+#/home/darkgaia/workroom/0prog/step1_galaxy_IC_preprocess/step3_preprocess_IC/step1_from_ascii_to_g1_and_run; rm *.exe; g++ read_snapshot.cpp ../../../Stackel/tact-master/DataInterface/Gadget2FormatData_io.cpp -o read_snapshot.exe; ./read_snapshot.exe 2 galaxy_general_simple.g1
